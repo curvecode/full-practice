f9f11699c91bb023571c3d350ce14a7f9b388b90
AAVN\ltthanh <thanh.le@axonactive.com>
2018-06-19 17:22:20 +0700

    Import style for reactjs
    - Import by file

--- ReactJS/react-material-ui/src/components/listTaskComp.js
+++ ReactJS/react-material-ui/src/components/listTaskComp.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+// import styles from '../styles/index.css';
+import Grid from '@material-ui/core/Grid';
+import TransMatComp from './transitionComp';
+
+// const style = {
+//     width: '640px',
+//     margin: '10px auto'
+// }
 
 export default class ListTaskComponent extends Component {
+    constructor(props) {
+        super(props);
+        console.log('Constructor APP');
+    }
     render() {
+        console.log('Render APP');
         return (
-            
+            <Grid container spacing={8}>
+                <ul xs={12} sm={6}>
+                    <li>Task 1</li>
+                    <li>Task 1</li>
+                    <li>Task 1</li>
+                    <li>Task 1</li>
+                    <li>Task 1</li>
+                </ul>
+                <ul xs={12} sm={6}>
+                    <li>Task 2</li>
+                    <li>Task 3</li>
+                    <li>Task 4</li>
+                    <li>Task 5</li>
+                    <li>Task 6</li>
+                </ul>
+                <TransMatComp abc={{ something: '123' }} />
+            </Grid>
         )
+    }
+    componentWillMount() {
+        console.log('Will mount');
+    }
+    componentDidUpdate() {
+        console.log('Did update');
     }
 };
--- ReactJS/react-material-ui/src/counterApp/index.js
+++ ReactJS/react-material-ui/src/counterApp/index.js
@@ -1,20 +1,22 @@
 /*jshint esversion: 6 */
 import React, { Component } from 'react';
-import CounterContainer from './containers/counterContainer';
-import TotalContainer from './containers/totalContainer';
-import InputNumberContainer from './containers/inputNumberContainer';
+// import CounterContainer from './containers/counterContainer';
+// import TotalContainer from './containers/totalContainer';
+// import InputNumberContainer from './containers/inputNumberContainer';
 import Navbar from '../components/navBarComponent';
-import SimpleCard from '../components/simpleCard';
+// import SimpleCard from '../components/simpleCard';
+import ListTask from '../components/listTaskComp';
 
 export default class CounterApp extends Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <CounterContainer />
-                <TotalContainer />
-                <InputNumberContainer />
-                <SimpleCard />
+                {/* <CounterContainer /> */}
+                {/* <TotalContainer /> */}
+                {/* <InputNumberContainer /> */}
+                {/* <SimpleCard /> */}
+                <ListTask />
             </div>
         )
     }
