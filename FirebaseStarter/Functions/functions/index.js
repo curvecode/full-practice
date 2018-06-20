fcf0c8b87edc3685eb02e69f28bcba5c3fa40453
AAVN\ltthanh <thanh.le@axonactive.com>
2018-06-20 16:15:14 +0700

    Firebase cloud function
    - Add & Get data from firestore of firebase

--- FirebaseStarter/Functions/functions/index.js
+++ FirebaseStarter/Functions/functions/index.js
@@ -33,43 +33,100 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 
 exports.getPosts = functions.https.onRequest((request, response) => {
     var docs = [];
-    db.collection('posts').get().then(snapshot => {
-        snapshot.forEach(doc => {
-            console.log(doc);
-            console.log(doc.data);
-            docs.push(doc.data); // _fieldsProto
+    var criteria = request.query || null;
+    // Get all document
+    // let promise = db.collection('posts');
+    // if (criteria) {
+    //     promise = promise.where('title', == , "title").get();
+    // } else {
+    //     promise = promise.get();
+    // }
+    var arrData = [];
+    db.collection('posts').get().then((snapshot) => {
+        snapshot.forEach((doc) => {
+            docs.push(doc.id);
+            arrData.push(doc.data());
         });
-        response.send(docs);
-    }).catch(error => {
+        docs.forEach(element => {
+            let tmp = db.collection('posts').doc(element);
+            tmp.onSnapshot(docSnapshot => {
+                console.log(`Received doc snapshot: ${docSnapshot.id} -- ${JSON.stringify(docSnapshot.data())}`);
+            }, err => {
+                console.log(`Encountered error: ${err}`);
+            });
+            // arrData.push(docSnapshot.data());
+        });
+        response.send(arrData);
+        // Promise.all(arrPromise).then(values => {
+        //     var arrData = [];
+        //     if (values.length > 0) {
+        //         values.forEach(snapData => {
+        //             if (snapData.exists) {
+        //                 arrData.push(snapData.data());
+        //             }
+        //         });
+        //     }
+        //     response.send(arrData);
+        // }).catch(error => {
+        //     response.send(error);
+        // });
+    }).catch((error) => {
         console.log(error);
         response.send('Error getting posts...');
-    })
-})
+    });
+});
 
 exports.addPost = functions.https.onRequest((request, response) => {
     console.log('Add post ....');
     let data = request.body;
 
     db.collection('posts').add(data).then(docRef => {
-        response.send(docRef);
+        console.log('Add document with id : ' + docRef.id);
+        response.send('Added post success with id : ' + docRef.id);
     }).catch(error => {
         response.send('Error ' + error);
-    })
-})
+    });
+
+    // var transaction = db.runTransaction(t => {
+    //     return t.get(db.collection('posts').doc(''))
+    // })
+});
+
+exports.deletePost = functions.https.onRequest((request, response) => {
+    console.log('##### Delete the post...');
+    // console.log(request.params['id']);
+    // console.log(request.baseUrl);
+    let id = request.query ? request.query.id : null;
+    console.log(`The post will be deleted : ${id}`);
+    if (id) {
+        db.collection('posts').doc(id).delete().then(res => {
+            console.log('Delete result: ' + JSON.stringify(res));
+            response.statusCode = 200;
+            response.statusMessage = 'Delete post success';
+            response.send({
+                message: 'The post was deleted'
+            });
+            // response.send(request);
+        }).catch(err => {
+            response.send(err);
+        });
+    } else {
+        response.statusCode = 400;
+        response.statusMessage = 'Bad request';
+        response.send(request);
+    }
+});
 
 exports.addCitizen = functions.https.onRequest((request, response) => {
     console.log('Add citizen');
+    var data = request.body;
 
     var citiesRef = db.collection('cities');
-    citiesRef.add({
-        first: 'Ada',
-        last: 'Lovelace',
-        born: 1815
-    }).then(docRef => {
-        response.send(docRef);
+    citiesRef.add(data).then(docRef => {
+        response.send(docRef.id);
     }).catch(error => {
         res.send('Error add citizen ' + error);
-    })
+    });
 
 });
 
@@ -77,9 +134,27 @@ exports.getCitizen = functions.https.onRequest((request, response) => {
     console.log('Get citizen');
 
     db.collection('cities').get().then(snapshot => {
-        response.send(snapshot);
+        var arrData = [];
+        snapshot.forEach(citizen => {
+            arrData.push(citizen.data());
+        });
+        response.send(arrData);
     }).catch(error => {
         response.send('Error ' + error);
-    })
-})
+    });
+
+    // Listen change on collection when something changed
+    db.collection('cities').onSnapshot(snapShot => {
+        let arrDocChanged = snapShot.docChanges;
+        if (arrDocChanged.length > 0) {
+            arrDocChanged.forEach(docChanged => {
+                console.log(`Something changed : ${JSON.stringify(docChanged.doc.data())} ::: ${docChanged.doc.exists}`);
+            });
+        }
+
+    }, err => {
+
+    });
+});
+
 // module.exports = { api_v1 };
